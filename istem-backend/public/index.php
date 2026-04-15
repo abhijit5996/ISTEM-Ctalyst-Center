8cac1775a3ca353bbcb162c1842a0d8fffa62309
abhijit5996 <abhijitdasRahul010304@gmai.com>
2026-04-15 09:06:02 +0530

    header fix in cors

--- istem-backend/public/index.php
+++ istem-backend/public/index.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+header("Access-Control-Allow-Origin: https://istem-ctalyst-center-1.onrender.com");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 define('LARAVEL_START', microtime(true));
 
@@ -14,7 +20,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
+/** @var \Illuminate\Foundation\Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$app->handleRequest(\Illuminate\Http\Request::capture());
